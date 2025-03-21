1a228672b29127d5e12259c1a789794f1adec65f
ogahsergesergehoo <sergeleo14@yahoo.fr>
2025-03-21 11:27:13 +0000

    ppo

--- smitci/settings.py
+++ smitci/settings.py
@@ -179,27 +179,27 @@ WSGI_APPLICATION = 'smitci.wsgi.application'
 #     }
 # }
 # local one
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'smitciv2',
-        'USER': 'postgres',
-        'PASSWORD': 'weddingLIFE18',
-        'HOST': 'localhost',
-        'PORT': '5433',
-    }
-}
-#prod
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.contrib.gis.db.backends.postgis',  # Correct engine for GIS support
-#         'NAME': os.environ.get('DATABASE_NAME'),
-#         'USER': os.environ.get('DATABASE_USER'),
-#         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-#         'HOST': os.environ.get('DATABASE_HOST'),
-#         'PORT': os.environ.get('DATABASE_PORT'),
+#         'ENGINE': 'django.contrib.gis.db.backends.postgis',
+#         'NAME': 'smitciv2',
+#         'USER': 'postgres',
+#         'PASSWORD': 'weddingLIFE18',
+#         'HOST': 'localhost',
+#         'PORT': '5433',
 #     }
 # }
+#prod
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',  # Correct engine for GIS support
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT'),
+    }
+}
 DBBACKUP_STORAGE = 'django.core.files.storage.FileSystemStorage'
 DBBACKUP_STORAGE_OPTIONS = {'location': os.path.join(BASE_DIR, 'dbbackup/')}
 
@@ -262,8 +262,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 #gdal-config --libs >---commande linux ou mac os
 
-GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH', '/opt/homebrew/opt/gdal/lib/libgdal.dylib')
-GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH', '/opt/homebrew/opt/geos/lib/libgeos_c.dylib')
+# GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH', '/opt/homebrew/opt/gdal/lib/libgdal.dylib')
+# GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH', '/opt/homebrew/opt/geos/lib/libgeos_c.dylib')
 LANGUAGES = [
     ('fr', 'Français'),
     ('en', 'English'),
